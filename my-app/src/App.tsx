5aaa5c4aff68aa3eef49a516eb44dac6c26d8edf
maxcouling <max.couling@outlook.com>
2022-07-25 19:24:24 +1200

    Images show up on main page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,15 @@ import axios from "axios";
 import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import TextField from "@mui/material/TextField";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel"
+// import TextField from "@mui/material/TextField";
+// import FormControl from "@mui/material/FormControl";
+// import InputLabel from "@mui/material/InputLabel"
 import Select, { SelectChangeEvent } from "@mui/material/Select"
 import MenuItem from "@mui/material/MenuItem"
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+import {TextField, InputLabel, FormControl, ImageList, ImageListItem, Typography, Box, AppBar, Toolbar} from "@mui/material";
+
 
 import Stack from '@mui/material/Stack';
 
@@ -22,6 +25,7 @@ import type {} from '@mui/x-date-pickers/themeAugmentation';
 
 import "./App.css";
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+
 
 
 function App() {
@@ -41,7 +45,7 @@ function App() {
     setRoverDate(newValue);
   };
   
-  const [roverInfo, setroverInfo] = useState<undefined | any>(undefined)
+  const [roverInfo, setroverInfo] = useState<undefined | string[]>(undefined)
   const ROVER_BASE_API_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
   const API_KEY = "hiy3UTIT1hXjjOhGekEKfFer0s8fSHsM7kIjG9s6"
   const {format} = require('date-fns');
@@ -51,26 +55,41 @@ function App() {
       <h1>Mars Rover Image Search</h1>
 
       <div>
-        
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Rover</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={roverName}
-            label="Rover"
-            onChange={handleChange}
-          >
-          <MenuItem value={"curiosity"}>Curiosity</MenuItem>
-          <MenuItem value={"opportunity"}>Opportunity</MenuItem>
-          <MenuItem value={"spirit"}>Spirit</MenuItem>
-        </Select>
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" 
+        sx={{
+          bgcolor: 'background.paper',
+          boxShadow: 1,
+          borderRadius: 2,
+          p: 2,
+          minWidth: 300,
+        }}>
+
+          <Toolbar>
+            
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Mars Rover Image Search
+            </Typography>
+          
+            <FormControl>
+            <InputLabel id="demo-simple-select-label">Rover</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={roverName}
+              label="Rover"
+              onChange={handleChange}
+            >
+            <MenuItem value={"curiosity"}>Curiosity</MenuItem>
+            <MenuItem value={"opportunity"}>Opportunity</MenuItem>
+            <MenuItem value={"spirit"}>Spirit</MenuItem>
+          </Select>
         </FormControl>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
     
         <DesktopDatePicker
           label="Date of Photos"
-          inputFormat="MM/dd/yyyy"
+          inputFormat="dd/MM/yyyy"
           value={roverDate}
           onChange={handleDateChange}
           renderInput={(params) => <TextField {...params} />}
@@ -86,10 +105,33 @@ function App() {
         >
           <SearchIcon style={{ fill: "blue" }} />
         </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+        
+        
       </div>
 
-      <p>You have entered {roverName}</p>
-
+        {(roverInfo && roverInfo.length !== 0)  ?
+        <ImageList cols={3} >
+      
+        {roverInfo.map((item) => (
+          <ImageListItem key={item}>
+            
+            <img
+              src={`${item}`}
+              alt={"The image g"}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+        :
+        <p>Not found</p>
+        
+        }
+      
+   
       
     </div>
   );
@@ -98,9 +140,10 @@ function App() {
     axios
       .get(ROVER_BASE_API_URL + roverName + "/photos?earth_date=" + `${format(roverDate, 'yyyy-MM-dd')}` + "&api_key=" + API_KEY)
       .then((res) => {
-        console.log(res.data);
+        setroverInfo(res.data.photos.map((item:{img_src:string})=>item.img_src));
       })
       .catch((err) => {
+        console.log(err)
         console.log("Rover information not found");
         setroverInfo(undefined);
       });
@@ -108,15 +151,3 @@ function App() {
 }
 
 export default App;
-//{roverName === undefined ? (
-//  <p>Rover not found</p>
-//  ) : (
-//    <div id="pokemon-result">
-//      {roverInfo.photos[0].img_src === null ? (
-//        <p>No image found</p>
-//      ) : (
-//        <img src={roverInfo.photos[0].img_src} />
-//      )}
-//      
-//    </div>
-//  )}
---
+++ my-app/src/Wrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function wrapper() {
+  return (
+    <div>wrapper</div>
+  )
+}
+
+export default wrapper
